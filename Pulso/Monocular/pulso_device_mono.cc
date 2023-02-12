3558db9a51750b35ff47f3614493ecc0b854fbfc
rallkaid <fermodil@mail.ru>
2023-02-12 14:31:23 +0300

    read from real cam

--- Pulso/Monocular/pulso_device_mono.cc
+++ Pulso/Monocular/pulso_device_mono.cc
@@ -36,13 +36,22 @@
 
 using namespace std;
 
-void LoadImages(const string &strImagePath, const string &strPathTimes,
-                vector<string> &vstrImages, vector<double> &vTimeStamps);
+// void LoadImages(const string &strImagePath, const string &strPathTimes,
+//                 vector<string> &vstrImages, vector<double> &vTimeStamps);
 
 const unsigned char *toChar(float x)
 {
     const unsigned char *pf = reinterpret_cast<const unsigned char *>(&x);
     return pf;
+}
+
+
+double now()
+{
+    auto time = std::chrono::system_clock::now().time_since_epoch();
+    std::chrono::seconds seconds = std::chrono::duration_cast<std::chrono::seconds>(time);
+    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(time);
+    return (double)seconds.count() + ((double)(ms.count() % 1000) / 1000.0);
 }
 
 int main(int argc, char **argv)
@@ -54,47 +63,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    const int num_seq = (argc - 3) / 2;
-    cout << "num_seq = " << num_seq << endl;
-    bool bFileName = (((argc - 3) % 2) == 1);
-    string file_name;
-    if (bFileName)
-    {
-        file_name = string(argv[argc - 1]);
-        cout << "file name: " << file_name << endl;
-    }
-
-    // Load all sequences:
-    int seq;
-    vector<vector<string>> vstrImageFilenames;
-    vector<vector<double>> vTimestampsCam;
-    vector<int> nImages;
-
-    vstrImageFilenames.resize(num_seq);
-    vTimestampsCam.resize(num_seq);
-    nImages.resize(num_seq);
-
-    int tot_images = 0;
-    for (seq = 0; seq < num_seq; seq++)
-    {
-        cout << "Loading images for sequence " << seq << "...";
-        LoadImages(string(argv[(2 * seq) + 3]) + "/mav0/cam0/data", string(argv[(2 * seq) + 4]), vstrImageFilenames[seq], vTimestampsCam[seq]);
-        cout << "LOADED!" << endl;
-
-        nImages[seq] = vstrImageFilenames[seq].size();
-        tot_images += nImages[seq];
-    }
-
-    // Vector for tracking time statistics
-    vector<float> vTimesTrack;
-    vTimesTrack.resize(tot_images);
-
-    cout << endl
-         << "-------" << endl;
-    cout.precision(17);
-
-    int fps = 20;
-    float dT = 1.f / fps;
+    cv::Mat myImage;
+    cv::VideoCapture cap(0);
 
     // UDP STUFF
     /////////////////
@@ -140,195 +110,75 @@ int main(int argc, char **argv)
     double t_resize = 0.f;
     double t_track = 0.f;
 
-    for (seq = 0; seq < num_seq; seq++)
+    while (true)
     {
 
-        // Main loop
-        cv::Mat im;
-        int proccIm = 0;
-        for (int ni = 0; ni < nImages[seq]; ni++, proccIm++)
-        {
-
-            // Read image from file
-            im = cv::imread(vstrImageFilenames[seq][ni], cv::IMREAD_UNCHANGED); //,CV_LOAD_IMAGE_UNCHANGED);
-            double tframe = vTimestampsCam[seq][ni];
-
-            if (im.empty())
-            {
-                cerr << endl
-                     << "Failed to load image at: "
-                     << vstrImageFilenames[seq][ni] << endl;
-                return 1;
-            }
-
-            if (imageScale != 1.f)
-            {
-#ifdef REGISTER_TIMES
-#ifdef COMPILEDWITHC14
-                std::chrono::steady_clock::time_point t_Start_Resize = std::chrono::steady_clock::now();
-#else
-                std::chrono::monotonic_clock::time_point t_Start_Resize = std::chrono::monotonic_clock::now();
-#endif
-#endif
-                int width = im.cols * imageScale;
-                int height = im.rows * imageScale;
-                cv::resize(im, im, cv::Size(width, height));
-#ifdef REGISTER_TIMES
-#ifdef COMPILEDWITHC14
-                std::chrono::steady_clock::time_point t_End_Resize = std::chrono::steady_clock::now();
-#else
-                std::chrono::monotonic_clock::time_point t_End_Resize = std::chrono::monotonic_clock::now();
-#endif
-                t_resize = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(t_End_Resize - t_Start_Resize).count();
-                SLAM.InsertResizeTime(t_resize);
-#endif
-            }
-
-#ifdef COMPILEDWITHC14
-            std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-#else
-            std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-#endif
-
-            // Pass the image to the SLAM system
-            // cout << "tframe = " << tframe << endl;
-            Sophus::SE3f res = SLAM.TrackMonocular(im, tframe); // TODO change to monocular_inertial
-            g2o::SE3Quat q = ORB_SLAM3::Converter::toSE3Quat(res);
-            // Eigen::Matrix<float, 4, 4> tr = res.matrix();
-
-            // float x = tr(0, 3);
-            // float y = tr(1, 3);
-            // float z = tr(2, 3);
-
-            Eigen::Quaterniond quat = q.rotation();
-            Eigen::Vector3d trans = q.translation();
-
-            float x = quat.x();
-            arr[0] = x;
-
-            float y = quat.y();
-            arr[1] = y;
-
-            float z = quat.z();
-            arr[2] = z;
-
-            float w = quat.w();
-            arr[3] = w;
-
-            float xt = trans.x();
-            arr[4] = xt;
-
-            float yt = trans.y();
-            arr[5] = yt;
-
-            float zt = trans.z();
-            arr[6] = zt;
-
-            printf("---------------------------------------\n");
-            cout << x << ", " << y << ", " << z << ", " << w << " | " << xt << ", " << yt << ", " << zt << endl;
-
-            int shift = 0;
-            int step = 4;
-            for (size_t i = 0; i < 7; i++)
-            {
-                const unsigned char *res = toChar(arr[i]);
-                for (size_t j = 0; j < step; j++)
-                {
-                    tempBuffer[(step - j - 1) + shift] = res[j];
-                }
-                shift = shift + step;
-            }
-
-            if (sendto(fd, tempBuffer, 28, 0, (struct sockaddr *)&addr,
-                       sizeof(addr)) < 0)
-            {
-                perror("sendto");
-                exit(1);
-            }
-#ifdef COMPILEDWITHC14
-            std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-#else
-            std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-#endif
-
-#ifdef REGISTER_TIMES
-            t_track = t_resize + std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(t2 - t1).count();
-            SLAM.InsertTrackTime(t_track);
-#endif
-
-            double ttrack = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
-
-            vTimesTrack[ni] = ttrack;
-
-            // Wait to load the next frame
-            double T = 0;
-            if (ni < nImages[seq] - 1)
-                T = vTimestampsCam[seq][ni + 1] - tframe;
-            else if (ni > 0)
-                T = tframe - vTimestampsCam[seq][ni - 1];
-
-            // std::cout << "T: " << T << std::endl;
-            // std::cout << "ttrack: " << ttrack << std::endl;
-
-            if (ttrack < T)
-            {
-                // std::cout << "usleep: " << (dT-ttrack) << std::endl;
-                usleep((T - ttrack) * 1e6); // 1e6
-            }
+        cap >> myImage;
+        if (myImage.empty())
+        { // Breaking the loop if no video frame is detected//
+            break;
         }
 
-        if (seq < num_seq - 1)
+        char c = (char)cv::waitKey(25); // Allowing 25 milliseconds frame processing time and initiating break condition//
+        if (c == 27)
+        { // If 'Esc' is entered break the loop//
+            break;
+        }
+
+        double timestamp = now();
+
+        Sophus::SE3f res = SLAM.TrackMonocular(myImage, timestamp); // TODO change to monocular_inertial
+        g2o::SE3Quat q = ORB_SLAM3::Converter::toSE3Quat(res);
+
+        Eigen::Quaterniond quat = q.rotation();
+        Eigen::Vector3d trans = q.translation();
+
+        float x = quat.x();
+        arr[0] = x;
+
+        float y = quat.y();
+        arr[1] = y;
+
+        float z = quat.z();
+        arr[2] = z;
+
+        float w = quat.w();
+        arr[3] = w;
+
+        float xt = trans.x();
+        arr[4] = xt;
+
+        float yt = trans.y();
+        arr[5] = yt;
+
+        float zt = trans.z();
+        arr[6] = zt;
+
+        printf("---------------------------------------\n");
+        cout << x << ", " << y << ", " << z << ", " << w << " | " << xt << ", " << yt << ", " << zt << endl;
+
+        int shift = 0;
+        int step = 4;
+        for (size_t i = 0; i < 7; i++)
         {
-            string kf_file_submap = "./SubMaps/kf_SubMap_" + std::to_string(seq) + ".txt";
-            string f_file_submap = "./SubMaps/f_SubMap_" + std::to_string(seq) + ".txt";
-            SLAM.SaveTrajectoryEuRoC(f_file_submap);
-            SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file_submap);
+            const unsigned char *res = toChar(arr[i]);
+            for (size_t j = 0; j < step; j++)
+            {
+                tempBuffer[(step - j - 1) + shift] = res[j];
+            }
+            shift = shift + step;
+        }
 
-            cout << "Changing the dataset" << endl;
-
-            SLAM.ChangeDataset();
+        if (sendto(fd, tempBuffer, 28, 0, (struct sockaddr *)&addr,
+                   sizeof(addr)) < 0)
+        {
+            perror("sendto");
+            exit(1);
         }
     }
     // Stop all threads
     SLAM.Shutdown();
     close(fd);
-
-    // Save camera trajectory
-    if (bFileName)
-    {
-        const string kf_file = "kf_" + string(argv[argc - 1]) + ".txt";
-        const string f_file = "f_" + string(argv[argc - 1]) + ".txt";
-        SLAM.SaveTrajectoryEuRoC(f_file);
-        SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
-    }
-    else
-    {
-        SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
-        SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
-    }
-
     return 0;
 }
 
-void LoadImages(const string &strImagePath, const string &strPathTimes,
-                vector<string> &vstrImages, vector<double> &vTimeStamps)
-{
-    ifstream fTimes;
-    fTimes.open(strPathTimes.c_str());
-    vTimeStamps.reserve(5000);
-    vstrImages.reserve(5000);
-    while (!fTimes.eof())
-    {
-        string s;
-        getline(fTimes, s);
-        if (!s.empty())
-        {
-            stringstream ss;
-            ss << s;
-            vstrImages.push_back(strImagePath + "/" + ss.str() + ".png");
-            double t;
-            ss >> t;
-            vTimeStamps.push_back(t * 1e-9);
-        }
-    }
-}
